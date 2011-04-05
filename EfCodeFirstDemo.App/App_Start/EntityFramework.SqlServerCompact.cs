fa0e0742de0e0d3b26a6024d45b15a0c2b1bc8d6
Chris Ortman <chrisortman@gmail.com>
2011-04-04 22:57:07 -0500

    First commit

---
+++ EfCodeFirstDemo.App/App_Start/EntityFramework.SqlServerCompact.cs
@@ -0,0 +1,11 @@
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+
+namespace EfCodeFirstDemo.App.App_Start {
+    public static class EntityFramework_SqlServerCompact {
+        public static void Start() {
+            Database.DefaultConnectionFactory = new SqlCeConnectionFactory("System.Data.SqlServerCe.4.0");
+        }
+    }
+}
---
+++ EfCodeFirstDemo.App/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace EfCodeFirstDemo.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DemoEntities();
+            var customer = new Customer {Name = "Chris", Birthday = new DateTime(1979, 12, 6)};
+            context.Customers.Add(customer);
+
+            context.SaveChanges();
+
+            Console.ReadLine();
+
+        }
+    }
+
+    internal class Customer
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}", ID, Name, Birthday);
+        }
+    }
+
+    internal class DemoEntities : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
