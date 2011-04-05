8149c5286181dddc227e6dec295dde53bf6b9457
Chris Ortman <chriso@innovsys.com>
2011-04-05 16:33:45 -0500

    Got db initializer working

--- EfCodeFirstDemo.App/Program.cs
+++ EfCodeFirstDemo.App/Program.cs
@@ -2,28 +2,48 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.IO;
 using System.Linq;
 using System.Text;
 using HibernatingRhinos.Profiler.Appender.EntityFramework;
 
 namespace EfCodeFirstDemo.App
 {
-    class Program
+    public class OrtmanFamilyInitializer : IDatabaseInitializer<FamilyMembers>
     {
-        static void Main(string[] args)
-        {
-   //         Database.DefaultConnectionFactory = new SqlCeConnectionFactory("System.Data.SqlServerCe.4.0");
-			EntityFrameworkProfiler.Initialize();
-            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<FamilyMembers>());
+        #region Implementation of IDatabaseInitializer<in FamilyMembers>
 
-            var context = new FamilyMembers();
+        /// <summary>
+        /// Executes the strategy to initialize the database for the given context.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public void InitializeDatabase(FamilyMembers context)
+        {
+            Seed(context);
+            context.SaveChanges();
+        }
+
+        #endregion
+
+        protected void Seed(FamilyMembers context)
+        {
+            var chris = context.Dads.Include(x => x.Kids).FirstOrDefault(x => x.Name == "Chris");
+            if(chris != null)
+            {
+                chris.Kids.Clear();
+                context.Dads.Remove(chris);
+            }
+
+            context.SaveChanges();
+
             var ethan = new Address()
             {
                 City = "Ethan",
                 State = "SD",
                 Zip = "57334"
             };
-            var chris = new Dad
+
+            chris = new Dad
             {
                 Name = "Chris", 
                 Birthday = new DateTime(1979, 12, 6),
@@ -45,15 +65,34 @@ namespace EfCodeFirstDemo.App
             };
 
             context.Dads.Add(chris);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			EntityFrameworkProfiler.Initialize();
+
+            //have to do this b4 profiler, since profiler doesn't like create / delete database
+            Database.SetInitializer(new OrtmanFamilyInitializer());
+            
+             PrintDads();
+
+            Console.WriteLine("========= after initialization ===========");
+            var context = new FamilyMembers();
+            
+            //since i know i've only just created 1 dad, i just hardcode the ID
+            var chris = context.Dads.First(x => x.Name == "Chris");
+
+            var clara = new Kid() {Name = "Clara", Birthday = DateTime.Parse("1/19/2010")};
+            chris.Kids.Add(clara);
 
             context.SaveChanges();
 
             PrintDads();
 
-            var clara = new Kid() {Name = "Clara", Birthday = DateTime.Parse("1/19/2010")};
-            chris.Kids.Add(clara);
-
-            PrintDads();
+            
 
             Console.WriteLine("All Done");
             Console.ReadLine();
